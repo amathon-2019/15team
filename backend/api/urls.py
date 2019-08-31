481c7bdc55d0c32c2aa279b8ad94a35979d3aa7a
miraedbswo <miraedbswo@gmail.com>
2019-08-31 20:28:31 +0900

    Add Auth urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('key/',views.KeyView.as_view()),
+    path('key/', views.KeyView.as_view()),
+    path('auth/register', views.RegistrationAPI.as_view()),
+    path('auth/login', views.LoginAPI.as_view()),
+    path('auth/user', views.UserAPI.as_view())
 ]
